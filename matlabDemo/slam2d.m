f555fc0b19b89140872630abdb7acefe421eafdb
tmandal1988 <tmandal1988@gmail.com>
2017-07-22 23:11:09 -0700

    Updated the code with Joan Sola m file

--- matlabDemo/slam2d.m
+++ matlabDemo/slam2d.m
@@ -1,126 +1,179 @@
-% SLAM2D A 2D EKF−SLAM algorithm with simulation and graphics.
-%
-% HELP NOTES:
-% 1. The robot state is defined by [xr;yr;ar] with [xr;yr] the position
-% and [ar] the orientation angle in the plane.
-% 2. The landmark states are simply Li=[xi;yi]. There are a number of N
-% landmarks organized in a 2−by−N matrix W=[L1 L2 ... Ln]
-% so that Li = W(:,i).
-% 3. The control signal for the robot is U=[dx;da] where [dx] is a forward
-% motion and [da] is the angle of rotation.
-% 4. The motion perturbation is additive Gaussian noise n=[nx;na] with
-% covariance Q, which adds to the control signal.
-% 5. The measurements are range−and−bearing Yi=[di;ai], with [di] the
-% distance from the robot to landmark Li, and [ai] the bearing angle from
-% the robot's x−axis.
-% 6. The simulated variables are written in capital letters,
-% R: robot
-% W: set of landmarks or 'world'
-% Y: set of landmark measurements Y=[Y1 Y2 ... YN]
-% 7. The true map is [xr;yr;ar;x1;y1;x2;y2;x3;y3; ... ;xN;yN]
-% 8. The estimated map is Gaussian, defined by
-% x: mean of the map
-% P: covariances matrix of the map
-% 9. The estimated entities (robot and landmarks) are extracted from {x,P}
-% via pointers, denoted in small letters as follows:
-% r: pointer to robot state. r=[1,2,3]
-% l: pointer to landmark i. We have for example l=[4,5] if i=1,
-% l=[6,7] if i=2, and so on.
-% m: pointers to all used landmarks.
-% rl: pointers to robot and one landmark.
-% rm: pointers to robot and all landmarks (the currently used map).
-% Therefore: x(r) is the robot state,
-% x(l) is the state of landmark i
-% P(r,r) is the covariance of the robot
-% P(l,l) is the covariance of landmark i
-% P(r,l) is the cross−variance between robot and lmk i
-% P(rm,rm) is the current full covariance −− the rest is
-% unused.
-% NOTE: Pointers are always row−vectors of integers.
-% 10. Managing the map space is done through the variable mapspace.
-% mapspace is a logical vector the size of x. If mapspace(i) = false,
-% then location i is free. Oterwise mapspace(i) = true. Use it as
-% follows:
-% * query for n free spaces: s = find(mapspace==false, n);
-% * block positions indicated in vector s: mapspace(s) = true;
-% * liberate positions indicated in vector s: mapspace(s) = false;
-% 11. Managing the existing landmarks is done through the variable landmarks.
-% landmarks is a 2−by−N matrix of integers. l=landmarks(:,i) are the
-% pointers of landmark i in the state vector x, so that x(l) is the
-% state of landmark i. Use it as follows:
-% * query 1 free space for a new landmark: i = find(landmarks(1,:)==0,1)
-% * associate indices in vector s to landmark i: landmarks(:,i) = s
-% * liberate landmark i: landmarks(:,i) = 0;
-% 12. Graphics objects are Matlab 'handles'. See Matlab doc for information.
-% 13. Graphic objects include:
-% RG: simulated robot
-% WG: simulated set of landmarks
-% rG: estimated robot
-% reG: estimated robot ellipse
-% lG: estimated landmarks
-% leG: estimated landmark ellipses
-% (c) 2010, 2011, 2012 Joan Sola.
+% SLAM algorithm (for comments see Joan Sola SLAM tutorial)
 
 % I. INITIALIZE
-% W: set of external landmarks
-% 
-W
 %
-N
-%
-R
-%
-U
-%
-Y I.1 SIMULATOR −− use capital letters for variable names
+%   0. System def.
 
-= cloister(−4,4,−4,4,7);
-% Type 'help cloister' for help
-N: number of landmarks
-= size(W,2);
-R: robot pose [x ; y ; alpha]
-= [0;−2;0];
-U: control [d x ; d alpha]
-= [0.1 ; 0.05]; % fixing advance and turn increments creates a circle
-Y: measurements of all landmarks
-= zeros(2, N);
+% System noise
+q = [0.01;0.005];
+Q = diag(q.^2);
+% Measurement noise
+m = [.15; 1*pi/180];
+M = diag(m.^2);
+% randn('seed',1);
+
 %
-%
-%
-x
-%
-P I.2 ESTIMATOR
-Map: Gaussian {x,P}
-x: state vector's mean
-= zeros(numel(R)+numel(W), 1);
-P: state vector's covariances matrix
-= zeros(numel(x),numel(x));
-% System noise: Gaussian {0,Q}
-q = [.01;.02];
-% amplitude or standard deviation
-Q = diag(q.ˆ2);
-% covariances matrix
-% Measurement noise: Gaussian {0,S}
-s = [.1;1*pi/180]; % amplitude or standard deviation
-S = diag(s.ˆ2);
-% covariances matrix
-% Map management
-mapspace = false(1,numel(x)); % See Help Note #10 above.
-% Landmarks management
-landmarks = zeros(2, N); % See Help Note #11 above
-% Place robot
-r
-=
-mapspace(r) =
-x(r)
-=
-P(r,r)
-=
-in map
-find(mapspace==false, numel(R) ); % set robot pointer
-true; % block map positions
-R;
-% initialize robot states
-0;
-% initialize robot covariance
-% I.3 GRAPHICS −− use the variable names of simulated and estimated
+%   1. Simulator
+%       R: robot pose u: control
+
+R = [0;-2.5;0];
+u = [0.1;0.05];
+W = cloister(-4,4,-4,4);
+y = zeros(2,size(W,2));
+
+%   2. Estimator
+x = zeros(numel(R)+numel(W), 1);
+P = zeros(numel(x),numel(x));
+mapspace = 1:numel(x);
+l = zeros(2, size(W,2));
+
+r = find(mapspace,numel(R));
+mapspace(r) = 0;
+x(r)   = R;
+P(r,r) = 0;
+
+%   3. Graphics
+mapFig = figure(1);
+cla;
+axis([-6 6 -6 6])
+axis square
+WG = line('parent',gca,...
+    'linestyle','none',...
+    'marker','o',...
+    'color','r',...
+    'xdata',W(1,:),...
+    'ydata',W(2,:));
+RG = line('parent',gca,...
+    'marker','.',...
+    'color','r',...
+    'xdata',R(1),...
+    'ydata',R(2));
+rG = line('parent',gca,...
+    'linestyle','none',...
+    'marker','+',...
+    'color','b',...
+    'xdata',x(r(1)),...
+    'ydata',x(r(2)));
+
+lG = line('parent',gca,...
+    'linestyle','none',...
+    'marker','+',...
+    'color','b',...
+    'xdata',[],...
+    'ydata',[]);
+
+eG = zeros(1,size(W,2));
+for i = 1:numel(eG)
+    eG(i) = line(...
+        'parent', gca,...
+        'color','g',...
+        'xdata',[],...
+        'ydata',[]);
+end
+
+reG = line(...
+    'parent', gca,...
+    'color','m',...
+    'xdata',[],...
+    'ydata',[]);
+
+% II. Temporal loop
+
+for t = 1:200
+    
+    % 1. Simulator
+    n = q.*randn(2,1);
+    R = move(R, u, zeros(2,1));
+    for lid = 1:size(W,2)
+        v = m.*randn(2,1);
+        y(:,lid) = project(R, W(:,lid)) + v;
+    end
+    
+    % 2. Filter
+    %   a. Prediction
+    %   CAUTION this is sub-optimal in CPU time
+    [x(r), R_r, R_n] = move(x(r), u, n);
+    P_rr = P(r,r);
+    P(r,:) = R_r*P(r,:);
+    P(:,r) = P(r,:)';
+    P(r,r) = R_r*P_rr*R_r' + R_n*Q*R_n';
+    
+    %   b. correction
+    %       i. known lmks
+    lids = find(l(1,:));
+    for lid = lids
+        % expectation
+        [e, E_r, E_l] = project(x(r), x(l(:,lid)));
+        E_rl = [E_r E_l];
+        rl   = [r l(:,lid)'];
+        E    = E_rl * P(rl,rl) * E_rl';
+        
+        % measurement
+        yi = y(:,lid);
+        
+        % innovation
+        z = yi - e;
+        if z(2) > pi
+            z(2) = z(2) - 2*pi;
+        end
+        if z(2) < -pi
+            z(2) = z(2) + 2*pi;
+        end
+        Z = M + E;
+        
+        % Kalman gain
+        K = P(:, rl) * E_rl' * Z^-1;
+        
+        % update
+        x = x + K * z;
+        P = P - K * Z * K';
+    end
+    
+    %       ii. init new lmks
+    % check lmk availability
+    lid = find(l(1,:)==0 , 1);
+    if ~isempty(lid)
+        s = find(mapspace, 2);
+        if ~isempty(s)
+            mapspace(s) = 0;
+            l(:,lid) = s';
+            % measurement
+            yi = y(:,lid);
+            
+            [x(l(:,lid)), L_r, L_y] = backProject(x(r), yi);
+            P(s,:) = L_r * P(r,:);
+            P(:,s) = P(s,:)';
+            P(s,s) = L_r * P(r,r) * L_r' + L_y * M * L_y';
+        end
+    end
+    
+    % 3. Graphics
+    
+    set(RG, 'xdata', R(1), 'ydata', R(2));
+    set(rG, 'xdata', x(r(1)), 'ydata', x(r(2)));
+    lids = find(l(1,:));
+    lx = x(l(1,lids));
+    ly = x(l(2,lids));
+    set(lG, 'xdata', lx, 'ydata', ly);
+    for lid = lids
+        le = x(l(:,lid));
+        LE = P(l(:,lid),l(:,lid));
+        [X,Y] = cov2elli(le,LE,3,16);
+        set(eG(lid),'xdata',X,'ydata',Y);
+    end
+    if t > 1
+        re = x(r(1:2));
+        RE = P(r(1:2),r(1:2));
+        [X,Y] = cov2elli(re,RE,3,16);
+        set(reG,'xdata',X,'ydata',Y);
+    end
+    drawnow;
+end
+
+
+
+
+
+
+
+
